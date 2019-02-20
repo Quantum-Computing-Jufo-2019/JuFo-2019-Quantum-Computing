8df21d11565313bd32a76004ffd1f5742c68a388
Jonathan Treffler <tessy.powder@gmail.com>
2019-02-20 13:31:34 +0100

    GUI 3.0

--- GUI/jufo-gui.py
+++ GUI/jufo-gui.py
@@ -18,20 +18,6 @@ import thread
 
 combobox_items =      ["N-Damen n=4","N-Damen n=5","N-Damen n=6","N-Damen n=7","N-Damen n=8","Knights Tour"]
 combobox_qubo_files = ["./Hamiltonians/qubomatrix_4_diagBesetzt.txt","./Hamiltonians/qubomatrix_5_diagBesetzt.txt","./Hamiltonians/qubomatrix_6_diagBesetzt.txt","./Hamiltonians/qubomatrix_7_diagBesetzt.txt","./Hamiltonians/qubomatrix_8_diagBesetzt.txt"]
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
 def qubo(matrix_text_file,num_reads_times_hundred,annealing_time):
 	qubomatrix = np.loadtxt(matrix_text_file)
 	print('Loaded matrix:\n', qubomatrix, '\n')
@@ -54,6 +40,8 @@ def qubo(matrix_text_file,num_reads_times_hundred,annealing_time):
 		responses.append(sampler.sample_qubo(qubo, num_reads=reads_per_request, annealing_time=annealing_time))
 
 		print('Saved result from request ',read,' in results.txt','   ',((i / (num_reads_times_hundred+0.0))*100))
+	ui.label.setVisible(False)
+
 def update_progress_bar():
 	if qubo_progress != 100:
 		print(qubo_progress)
@@ -66,9 +54,22 @@ def start():
 	print("Annealing Time: "+str(ui.annealing_slider.value()))
 	print("Hamiltonian: "+str(combobox_items[ui.comboBox_2.currentIndex()]))
 	#ui.progressBar.setVisible(True)
-	ui.progressLabel.setVisible(True)
+	ui.label.setVisible(True)
 	thread.start_new_thread(qubo,(combobox_qubo_files[ui.comboBox_2.currentIndex()],(ui.durchlaufe_slider.value()/100),ui.annealing_slider.value()))
 	#update_progress_bar()
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_JufoQubo(object):
     def setupUi(self, JufoQubo):
@@ -88,8 +89,8 @@ class Ui_JufoQubo(object):
         self.verticalLayout_3 = QtGui.QVBoxLayout()
         self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
         self.comboBox_2 = QtGui.QComboBox(self.verticalLayoutWidget)
-        self.comboBox_2.setObjectName(_fromUtf8("comboBox_2"))
         self.comboBox_2.addItems(combobox_items)
+        self.comboBox_2.setObjectName(_fromUtf8("comboBox_2"))
         self.verticalLayout_3.addWidget(self.comboBox_2)
         self.horizontalLayout_5 = QtGui.QHBoxLayout()
         self.horizontalLayout_5.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
@@ -144,25 +145,18 @@ class Ui_JufoQubo(object):
         self.verticalLayout_3.addLayout(self.horizontalLayout_4)
         spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
         self.verticalLayout_3.addItem(spacerItem)
-        
-		self.progressLabel = QtGui.QLabel(self.verticalLayoutWidget)
-		self.progressLabel.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTop|QtCore.Qt.AlignTrailing)
-		self.progressLabel.setObjectName(_fromUtf8("progressLabel"))
-        self.progressLabel.setVisible(False)
-        self.verticalLayout_3.addWidget(self.progressLabel)
-        
-        #self.progressBar = QtGui.QProgressBar(self.verticalLayoutWidget)
-        #self.progressBar.setProperty("value", 0)
-        #self.progressBar.setObjectName(_fromUtf8("progressBar"))
-        #self.progressBar.setVisible(False)
-        #self.verticalLayout_3.addWidget(self.progressBar)
+        self.label = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label.setVisible(False)
+        self.verticalLayout_3.addWidget(self.label)
         self.Start = QtGui.QPushButton(self.verticalLayoutWidget)
         self.Start.setCheckable(False)
         self.Start.setAutoDefault(False)
         self.Start.setDefault(False)
         self.Start.setFlat(False)
-        self.Start.setObjectName(_fromUtf8("Start"))
         self.Start.clicked.connect(start)
+        self.Start.setObjectName(_fromUtf8("Start"))
         self.verticalLayout_3.addWidget(self.Start)
         self.verticalLayout_2.addLayout(self.verticalLayout_3)
         JufoQubo.setCentralWidget(self.main)
@@ -172,14 +166,15 @@ class Ui_JufoQubo(object):
 
     def retranslateUi(self, JufoQubo):
         JufoQubo.setWindowTitle(_translate("JufoQubo", "Jufo Qubo", None))
-        self.progressLabel.setText(_translate("JufoQubo", "Berechnung läuft", None))
         self.durchlaufe.setText(_translate("JufoQubo", "Durchläufe", None))
         self.annealingTime.setText(_translate("JufoQubo", "Annealing Time", None))
         self.durchlaufe_min.setText(_translate("JufoQubo", "1000", None))
         self.durchlaufe_max.setText(_translate("JufoQubo", "10.000", None))
         self.annealing_min.setText(_translate("JufoQubo", "20", None))
         self.annealing_max.setText(_translate("JufoQubo", "80", None))
+        self.label.setText(_translate("JufoQubo", "Berechnung läuft", None))
         self.Start.setText(_translate("JufoQubo", "Start", None))
+
 
 if __name__ == "__main__":
     import sys
@@ -189,12 +184,5 @@ if __name__ == "__main__":
     ui.setupUi(JufoQubo)
     JufoQubo.show()
     sys.exit(app.exec_())
-	
 qubo_progress = 0
-	
-		#for response in responses:
-			#for sample, energy, num_occurrences, cbf in response.record:
-				#file.write('%f\t%d\t%s' % (energy, num_occurrences, np.array2string(sample, max_line_width=None).replace('\n','')))
-				#file.write('\n')
 
-	#return(responses)
